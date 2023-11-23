8519b8988a142846788e28008e8db2df0b07ce5c
SilvesterSpath <spath.sz@gmail.com>
2023-11-23 10:12:18 +0100

    passportjs config

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const hbs = require('hbs');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: '.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 // Connect DB
 connectDB();
@@ -20,6 +25,19 @@ if (process.env.NODE_ENV === 'development') {
 
 // Handlebars
 app.set('view engine', '.hbs');
+
+// Set session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Set passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
