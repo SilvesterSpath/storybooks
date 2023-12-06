cca56a6c353e4ec1890b59b641ad7d68ace9aa93
SilvesterSpath <spath.sz@gmail.com>
2023-12-06 13:58:04 +0100

    ensureGuest ensureAuth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 // @desc Login/Landing page
 // @route GET /
-router.get('/', (req, res) => {
+router.get('/', auth.ensureGuest, (req, res) => {
   res.render('loginview', { layout: 'layouts/login' });
 });
 
@@ -13,7 +14,7 @@ router.get('/header', (req, res) => {
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', auth.ensureAuth, (req, res) => {
   res.render('dashboard', { layout: 'layouts/main' });
 });
 
