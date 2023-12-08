f8682214d025bbdccd84855756f7381dd3891185
SilvesterSpath <spath.sz@gmail.com>
2023-12-08 08:11:51 +0100

    hbs helper funcions

---
+++ helpers/hbs.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+
+module.exports ={
+  formatData: function(data, ){
+return moment(data).format(format);
+}
--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/dashboard', auth.ensureAuth, async (req, res) => {
   try {
     // we should use .lean() to be able to render it
     const stories = await Story.find({ user: req.user.id }).lean();
-    res.render('dashboard', { name: req.user.lastName, stories });
+    res.render('dashboard', { name: req.user.displayName, stories });
   } catch (error) {
     console.error(error);
     res.render('error/500', { error });
