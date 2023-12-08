fd2df384fe864af998644e7cf77e8d20b289dc2a
SilvesterSpath <spath.sz@gmail.com>
2023-12-08 08:12:12 +0100

    hbs helper functions

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 
-module.exports ={
-  formatData: function(data, ){
-return moment(data).format(format);
-}
+module.exports = {
+  formatData: function (data) {
+    return moment(data).format(format);
+  },
+};
